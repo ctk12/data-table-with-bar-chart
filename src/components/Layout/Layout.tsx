3f65816bd94936ac7616435f6e8016b69ad20b22
chetan <chetankatwa12@gmail.com>
2024-01-18 21:58:38 +0530

    Update

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -6,7 +6,7 @@ export const Layout = ({children}: {children: ReactNode}) => {
   return (
     <div className="flex flex-col h-screen w-full relative">
         <Header name="All Users" />
-        <main className="main w-full max-[900px]:p-3 p-10 flex justify-center">
+        <main className="main w-full max-[900px]:p-3 max-[900px]:pb-10 p-10 flex justify-center">
            <div className="main__wrap">
              {children}
            </div>
